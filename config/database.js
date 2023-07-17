3178d7e74b1db2a3b0fabe9240af3d2b6c0a91ac
Mohd Faizan <31098815+M0hdF41Z4N@users.noreply.github.com>
2023-07-17 11:00:35 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 // Importing mongoose
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://root:toor@cluster0.qghc4gy.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://user:password@cluster0.qghc4gy.mongodb.net/?retryWrites=true&w=majority";
 
 main().catch(err => console.log(err));
 
 async function main() {
+  // For local DB
   // await mongoose.connect('mongodb://127.0.0.1:27017/social_media_db');
   await mongoose.connect(uri);
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 // For debugging purpose
